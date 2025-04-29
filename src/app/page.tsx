619ee0dd9b5d692d2a324ec2a53caaedd6bffae9
ryuyafujisaki <ryuya.fujisaki@gmail.com>
2025-04-29 21:16:47 +0900

    created sign-in & sign-up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
 import { UserButton } from "@clerk/nextjs";
-import { currentUser } from "@clerk/nextjs/server";
+import { auth, currentUser } from "@clerk/nextjs/server";
 
 export default async function HomePage() {
-  const user = await currentUser();
+  const { userId } = await auth();
+
+  // Protect the route by checking if the user is signed in
+  if (!userId) {
+    return <div>Sign in to view this page</div>
+  }
+
+  // Get the Backend API User object when you need access to the user's information
+  const user = await currentUser()
+
   return (
     <div>
       <h1>ようこそ、{user?.firstName}さん。</h1>
